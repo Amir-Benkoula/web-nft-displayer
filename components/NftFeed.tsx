b42c64d04088fd04dd3467a07a858658a183c7e5
Amir Benkoula <amirbenkoula@MacBook-Pro-de-Amir.local>
2023-05-02 01:29:13 +0200

    pagination

--- components/NftFeed.tsx
+++ components/NftFeed.tsx
@@ -1,5 +1,69 @@
-import Image from "next/image"
-import { stringify } from "querystring";
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import styles from '../styles/Home.module.css'
+
+export default function NftFeed({ nfts }: any) {
+  const [pageNumber, setPageNumber] = useState(0);
+  const nftsPerPage = 20;
+  const pagesVisited = pageNumber * nftsPerPage;
+
+  const displayNfts = nfts
+    ? nfts
+        .slice(pagesVisited, pagesVisited + nftsPerPage)
+        .map((nft: any) => <NftItem nft={nft} key={nft.id} />)
+    : null;
+
+  const pageCount = Math.ceil(nfts.length / nftsPerPage);
+
+  const changePage = ({ selected }: { selected: number }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <div className={styles.container}>
+      <ReactPaginate
+        nextLabel="next >"
+        pageCount={pageCount}
+        onPageChange={changePage}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination justify-content-center"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+      <div className={styles.feed}>{displayNfts}</div>
+    </div>
+  );
+}
+
+function NftItem({ nft }: any) {
+  return (
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        src={nft.media[0].gateway}
+        alt="Picture of the 20Mint NFT"
+      />
+      <p>{nft.rawMetadata.name}</p>
+      <ul className={styles.attributes}>
+        {nft.rawMetadata.attributes.map((attribute: any, i: number) => {
+          return attribute.value !== 'None' ? (
+            <li key={i}>
+              {attribute.trait_type} : {attribute.value}
+            </li>
+          ) : null;
+        })}
+      </ul>
+    </div>
+  );
+}
 
 // type NftType = {
 //     rawMetadata: {
@@ -10,25 +74,28 @@ import { stringify } from "querystring";
 //     }[]
 // }
 
-export default function NftFeed({ nfts }: any) {
-    return nfts
-      ? nfts.map((nft: any) => (
-          <NftItem nft={nft} key={nft.id}/>
-        ))
-      : null;
-  }
+// export default function NftFeed({ nfts }: any) {
+//     return nfts
+//       ? 
+//       <div className={styles.feed}>
+//         {nfts.map((nft: any) => (
+//         <NftItem nft={nft} key={nft.id}/>
+//         ))}
+//       </div>
+//       : null;
+//   }
   
 
-function NftItem({nft}: any) {
-    return (
-        <div className="card">
-            <p>{nft.rawMetadata.name}</p>
-            <img src={nft.media[0].gateway} alt="Picture of the 20Mint NFT" width={200} height={200}/>
-            <ul>    
-                {nft.rawMetadata.attributes.map((attribute: any, i: number) => {
-                    return attribute.value != 'None' ? <li key={i}>{attribute.trait_type} : {attribute.value}</li> : null;
-                })}
-            </ul>
-        </div>
-    )
-}
+// function NftItem({nft}: any) {
+//     return (
+//         <div className={styles.card}>
+//             <img className={styles.image} src={nft.media[0].gateway} alt="Picture of the 20Mint NFT"/>
+//             <p>{nft.rawMetadata.name}</p>
+//             <ul className={styles.attributes}>    
+//                 {nft.rawMetadata.attributes.map((attribute: any, i: number) => {
+//                     return attribute.value != 'None' ? <li key={i}>{attribute.trait_type} : {attribute.value}</li> : null;
+//                 })}
+//             </ul>
+//         </div>
+//     )
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import React from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Network, Alchemy } from 'alchemy-sdk'
 import NftFeed from '../components/NftFeed'
@@ -35,22 +35,16 @@ export default function Home(props: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header>
+        <h1>20Mint NFT Displayer</h1>
+      </header>
+
       <main className={styles.main}>
         <NftFeed nfts={props.nfts}/>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <footer className={styles.footer}>
+      </footer> */}
     </div>
   )
 }
